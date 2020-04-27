f53e6338d4ebf74fe7a7ba5ad9eecf0568e82858
AbiYazid <eskrimtemulawak@gmail.com>
2020-04-27 23:50:47 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -9,4 +9,14 @@ public class OperatorBinary {
     String nama = "Abi Yazid Busthomi";
     int jumlahSks = 24;
     double IPK = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
 }
