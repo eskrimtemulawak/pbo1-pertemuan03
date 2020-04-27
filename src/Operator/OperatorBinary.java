53bf6af2a4c6d29ee708c1d8778d4095d4a45789
AbiYazid <eskrimtemulawak@gmail.com>
2020-04-27 23:48:32 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    /* operator (=) sebagai assignment/penugasan
+       menempatkan nilai kedalam variabel
+       baris 5-7 adalah cooment/komentar yang tidak dijalankan oleh program*/
+
+    String nama = "Abi Yazid Busthomi";
+    int jumlahSks = 24;
+    double IPK = 3.89;
+}
